bf0ff6fb140a717c24352e69ed2324470fd58114
Muhammet <mcoskun86@gmail.com>
2022-04-05 10:32:32 +0300

    First Commit

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const Layout: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1>MC</h1>
+      <Navigation />
+    </React.Fragment>
+  );
+};
+
+export default Layout;
---
+++ src/components/Layout/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Navigation: React.FC = () => {
+  return (
+    <header>
+      <div>falan</div>
+    </header>
+  );
+};
+
+export default Navigation;
