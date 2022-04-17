e423dfc2bab288b2c020c30b6b2b34dba7460b93
Muhammet <mcoskun86@gmail.com>
2022-04-17 12:51:26 +0300

    Create About Page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import Image from "next/image";
-import * as image from "./mami.jpeg";
 import PageHeading from "../../src/components/Layout/PageHeading";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import AboutPageContent from "./AboutPageContent";
 
 const About: React.FC = () => {
   return (
@@ -15,36 +13,7 @@ const About: React.FC = () => {
       }}
     >
       <PageHeading> me</PageHeading>
-      <Box
-        sx={{
-          display: { xs: "block", md: "flex" },
-          flexDirection: "row",
-        }}
-      >
-        <Box sx={{ width: "100%" }}>
-          <Typography variant="h5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do //
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut //
-            enim ad minim veniam, quis nostrud exercitation ull
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            width: "100%",
-            justifyContent: "center",
-            textAlign: "center",
-            my: 2,
-          }}
-        >
-          <Image
-            src={image}
-            alt="mami"
-            width="250"
-            height="300"
-            style={{ borderRadius: "50%" }}
-          />
-        </Box>
-      </Box>
+      <AboutPageContent />
     </Box>
   );
 };
